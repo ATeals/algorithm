1199681d634ceb22c6addeb4bf68d1d6f6539a15
A_Teals <125727432+ATeals@users.noreply.github.com>
2023-07-09 11:34:08 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -1,3 +1,5 @@
 const solution = (arr) => arr.length >10 ? 
     arr.reduce((acc,crr)=> (acc+crr) , 0) : 
     arr.reduce((acc,crr)=> (acc * crr) , 1)
+
+// const solution (arr) =>  arr.reduce((acc,crr)=> arr.length > 10?(acc+crr) : acc * crr)
