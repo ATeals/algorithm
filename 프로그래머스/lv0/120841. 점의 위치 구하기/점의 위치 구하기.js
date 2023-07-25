3fe46d43c3149f462beb0acc7cae47fa0e4ccda9
A_Teals <125727432+ATeals@users.noreply.github.com>
2023-07-25 16:50:18 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,6 +1,1 @@
-const solution = (dot) => {
-    if ((dot[0] > 0) && (dot[1] > 0)) return 1
-    if ((dot[0] < 0) && (dot[1] > 0)) return 2
-    if ((dot[0] < 0 )&& (dot[1] < 0 )) return 3
-    if ((dot[0] > 0 )&& (dot[1] < 0)) return 4
-}
+const solution = ([x,y]) => (x > 0) ? ((y > 0 )? 1 : 4) : ((y > 0 )? 2 : 3) 
