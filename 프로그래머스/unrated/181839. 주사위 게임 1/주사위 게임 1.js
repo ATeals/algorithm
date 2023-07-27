bfe015b2d017f209cdff0b9011918d9afcf67046
A_Teals <125727432+ATeals@users.noreply.github.com>
2023-07-28 03:39:35 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,9 @@
+function solution(a, b) {
+    if(a %2 === 0 && b %2 === 0){
+        return a > b ? a-b : b-a
+    }else if(a %2 === 1 && b %2 === 1){
+        return  a * a + b * b
+    }else{
+        return 2 * (a+b)
+    }
+}
