1efc98981dbee405c197dab9221eadacf843ebba
A_Teals <125727432+ATeals@users.noreply.github.com>
2023-08-04 17:08:01 +0900

    Update 개인정보 수집 유효기간.js

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -10,27 +10,10 @@ const solution = (today, terms, privacies) =>{
         let [날짜,약관] = crr.split(" ")
         날짜 = new Date(날짜)
         
-
-//         if((Math.abs(오늘.getDate() - 날짜.getDate()) === 0) && getMonth(날짜, 오늘) == 유효기간[약관]){
-//             return [...acc, index+1]
-//         }
-        
-//         return (getMonth(날짜, 오늘) > 유효기간[약관]) ? [...acc, index+1] : acc
-        
         날짜.setMonth(날짜.getMonth() + 유효기간[약관]) 
         
         return 날짜 <= 오늘 ? [...acc, index+1] : acc
         
-}, [])
+    }, [])
     
 } 
-
-const getMonth = (start, end) => {
-
-  const yearDiff = end.getFullYear() - start.getFullYear();
-  const monthDiff = end.getMonth() - start.getMonth();
-
-  const totalMonths = yearDiff * 12 + monthDiff;
-
-  return totalMonths;
-}
