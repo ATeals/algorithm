0564016586c8c8b52867a1fcd8687b498a17145e
A_Teals <125727432+ATeals@users.noreply.github.com>
2023-08-22 10:39:07 +0900

    [unrated] Title: 덧칠하기, Time: 4.90 ms, Memory: 40.1 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,3 +1,2 @@
-const solution = (n, m, section)=> section.reduce(([end,count],c)=>{
-    return end < c ? [c+m-1, ++count] : [end,count]
-}, [0,0])[1]
+const solution = (n, m, section)=> section.reduce(([end,count],c)=>end < c ? [c+m-1, ++count] : [end,count]
+, [0,0])[1]
