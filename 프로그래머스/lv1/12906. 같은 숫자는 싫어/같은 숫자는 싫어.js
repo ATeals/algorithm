e75096ba33b47890c399e5dc745312d4264578b7
A_Teals <125727432+ATeals@users.noreply.github.com>
2023-09-11 12:24:46 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.25 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+const solution = (arr) => {
+    const result = [arr[0]]
+    
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] === arr[i-1]) continue
+            
+        result.push(arr[i])
+    }
+    
+    return result
+}
