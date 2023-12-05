13c0358c967ea09af25a814543e1d5a3bb8cab05
A_Teals <125727432+ATeals@users.noreply.github.com>
2023-12-05 09:26:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.10 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,57 +1,36 @@
-// const d = [
-//   [1, 0],
-//   [-1, 0],
-//   [0, 1],
-//   [0, -1],
-// ];
-
-// function solution(maps) {
-//   const [W, H] = [maps.length - 1, maps[0].length - 1];
-
-//   const queue = [[0, 0, 0]];
-
-//   while (queue.length > 0) {
-//     const [X, Y, C] = queue.shift()
-
-//     if (maps[X][Y] === 1) maps[X][Y] += C;
-
-//     for (let [dx, dy] of d) {
-//       const [x, y] = [X + dx, Y + dy];
-//       if (x < 0 || y < 0 || x > W || y > H) continue;
-
-//       if (maps[x][y] === 1) {
-//         queue.push([x, y, C + 1]);
-//       }
-//     }
-//   }
-    
-//   const fin = maps[W][H] 
-
-//   return fin=== 1 ? -1 : fin
-// }
-
+const d = [
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+];
 
 function solution(maps) {
-    const dx = [0, 0, 1, -1];
-    const dy = [1, -1, 0, 0];
-    const queue = [[0, 0, 1]];
+  const [W, H] = [maps.length - 1, maps[0].length - 1];
 
-    while (queue.length) {
-        const cur = queue.shift();
-        if (cur[0] === maps.length - 1 && cur[1] === maps[0].length - 1) {
-            return cur[2];
-        }
-            
-        for (let i = 0; i < 4; i++) {
-            const ny = cur[0] + dy[i];
-            const nx = cur[1] + dx[i];
-            
-            if (nx >= 0 && ny >= 0 && nx < maps[0].length && ny < maps.length && maps[ny][nx] === 1) {
-                maps[ny][nx] = 0;
-                queue.push([ny, nx, cur[2] + 1]);    
-            }
-        }
+  const queue = [[0, 0, 0]];
+
+  while (queue.length > 0) {
+    const [X, Y, C] = queue.shift()
+        
+    if (maps[X][Y] === 1) maps[X][Y] += C;
+      
+
+
+    for (let [dx, dy] of d) {
+      const x = X + dx;
+      const y = Y + dy;
+      if (x < 0 || y < 0 || x > W || y > H) continue;
+      
+      if(x === W && y === H) return C+2
+
+      if (maps[x][y] !== 0) {
+        queue.push([x, y, C + 1]);
+        maps[x][y] = 0
+      }
     }
+  }
     
-    return -1;
+  return -1
 }
+
