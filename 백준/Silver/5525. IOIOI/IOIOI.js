39d58eeecd85467ed08c8d6367251f0845834194
ATeals <125727432+ATeals@users.noreply.github.com>
2023-12-31 15:56:41 +0900

    [Silver I] Title: IOIOI, Time: 220 ms, Memory: 15452 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -10,13 +10,22 @@ const input = fs
 
 const N = Number(input.shift());
 const str = input[1];
+const M = str.length;
 
-const P = "IO".repeat(N) + "I";
+let result = 0;
+let count = 0;
 
-let c = 0;
-
-for (let i = 0; i < str.length - P.length + 1; i++) {
-  if (str.slice(i, i + P.length) === P) c++;
+for (let i = 0; i < M - 2; i++) {
+  if (str.slice(i, i + 3) === "IOI") {
+    i += 1;
+    count += 1;
+    if (count === N) {
+      result += 1;
+      count -= 1;
+    }
+  } else {
+    count = 0;
+  }
 }
 
-console.log(c);
+console.log(result);
